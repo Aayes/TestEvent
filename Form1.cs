0f0a931439b82e5fb9e32b396a311b5ca5f9c5d0
zhangroot <zhangqw@v-t.net.cn>
2020-01-08 08:54:51 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 测试委托
+{
+    public partial class Form1 : Form
+    {
+        Task task = new Task();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            task.EventTxFarms += Task_EventTxFarms;
+        }
+
+        private void Task_EventTxFarms(object sender, EventArgs e)
+        {
+            double time = (task.st.ElapsedTicks / (double)Stopwatch.Frequency * 1000);
+            Console.WriteLine(time);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            task.start();
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace 测试委托
+{
+    class Task
+    {
+        public Task()
+        {
+            st.Start();
+        }
+        public Stopwatch st = new Stopwatch();
+        public event EventHandler<EventArgs> EventTxFarms;
+        private void TxFarmsEvent(int id, byte[] dat, int dlc, long time)
+        {
+            if (EventTxFarms != null)
+            {
+                EventArgs e_args = new EventArgs();
+                EventTxFarms(this, e_args);
+            }
+        }
+        public void start()
+        {
+            st.Restart();
+            TxFarmsEvent(0x7df, new byte[8], 8, 10);
+        }
+    }
+}
